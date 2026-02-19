062d30c0534c5335f3ea917b2793a17fa4b4d029
Ryan Bright <ryan@moonriseconsulting.io>
2026-02-19 13:27:29 -0700

    fix(module): drop deprecated docker enableNvidia option

--- flake.nix
+++ flake.nix
@@ -170,7 +170,6 @@
               docker = {
                 enable = true;
                 enableOnBoot = true;
-                enableNvidia = true;
               };
               oci-containers = {
                 backend = "docker";
